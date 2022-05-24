0b3099e3ef515d7fecf45ceb28b3731b8388e13f
Ruben <rubenbotha04@gmail.com>
2022-05-24 10:19:04 +1000

    Updated Level Spawning System

--- Lever Spawner.gd
+++ Lever Spawner.gd
@@ -6,6 +6,8 @@ var checkpoint = preload("res://Levels/Check Point.tscn")
 
 var grass_levels = [preload("res://Levels/Grass_Level_1.tscn"),preload("res://Levels/Grass_Level_2.tscn"),preload("res://Levels/Grass_Level_3.tscn"),preload("res://Levels/Grass_Level_4.tscn"),preload("res://Levels/Grass_Level_5.tscn"),preload("res://Levels/Grass_Level_6.tscn")]
 
+var levels_to_spawn = []
+
 var loaded_levels = []
 
 
@@ -23,12 +25,19 @@ func spawn_checkpoint():
 
 func spawn_level():
 
-	if grass_levels.size() >= 1:
-		grass_levels.shuffle()
-		var x = grass_levels.front()
+	if levels_to_spawn.size() == 0:
+		levels_to_spawn.append_array(grass_levels)
+		levels_to_spawn.shuffle()
+		print(levels_to_spawn)
+		#spawn_level()
+
+	if levels_to_spawn.size() >= 1:
+		levels_to_spawn.shuffle()
+		var x = levels_to_spawn.front()
 		#var x = preload("res://Levels/Grass_Level_6.tscn")
-		#grass_levels.erase(x) 
+		levels_to_spawn.erase(x) 
 		var spawninglevel = x.instance()
+		print (levels_to_spawn)
 		spawninglevel.translation.x = Global.map_length * 11.5
 		add_child(spawninglevel)
 		spawn_checkpoint()
